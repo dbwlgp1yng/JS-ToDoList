5016dbd653eeed82b9d5ee4cc1fe41f59cb414be
jihye yu <dbwlgp1yng@gmail.com>
2023-06-23 13:01:54 +0900

    이벤트위임 적용

--- js/main.js
+++ js/main.js
@@ -17,34 +17,31 @@ function onAdd(){
     addInput.focus();
 }
 
+let id = 0; // UUID
 function createItem(text) {
     const itemRow = document.createElement('li');
     itemRow.classList.add('item_row');
-
-    const item = document.createElement('div');
-    item.classList.add('item');
-
-    const name = document.createElement('span');
-    name.classList.add('item_name');
-    name.innerText = text;
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.classList.add('item_delete');
-    deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>'; 
-    deleteBtn.addEventListener('click', () => {
-        items.removeChild(itemRow);
-    });
-
-    const itemDivider = document.createElement('div');
-    itemDivider.classList.add('item_divider');
-
-    item.appendChild(name);
-    item.appendChild(deleteBtn);
-    itemRow.appendChild(item);
-    itemRow.appendChild(itemDivider);
-
+    itemRow.setAttribute('data-id', id);
+    itemRow.innerHTML = `
+        <div class="item">
+            <span class="item_name">${text}</span>
+            <button class="item_delete">
+                <i class="fa-solid fa-trash" data-id=${id}></i>
+            </button>
+        </div>
+        <div class="item_divider"></div>
+    `;
+    id++;
     return itemRow;
 }
+
+items.addEventListener('click', event => {
+    const id = event.target.dataset.id;
+    if(id){
+        const toBeDeleted = document.querySelector(`.item_row[data-id="${id}"]`);
+        toBeDeleted.remove();
+    }
+});
 
 addInput.addEventListener('keypress', event => {
     if(event.key === 'Enter') {
